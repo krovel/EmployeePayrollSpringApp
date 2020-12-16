f750ee119212dd5bbdaa01bb596fe20330d641ba
krove <krove@DESKTOP-Q5SED1R>
2020-12-17 03:47:11 +0530

    [Kashif]Added: Service Layer to the Employee Payroll App

---
+++ src/main/java/com/cg/employeepayroll/services/IEmployeePayrollService.java
@@ -0,0 +1,20 @@
+package com.cg.employeepayroll.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.cg.employeepayroll.dto.EmployeePayrollDTO;
+import com.cg.employeepayroll.model.EmployeePayrollData;
+
+public interface IEmployeePayrollService {
+
+	List<EmployeePayrollData> getEmployeePayrollData();
+
+	EmployeePayrollData getEmployeePayrollDataById(int empId);
+
+	EmployeePayrollData createEmployeePayrollData(EmployeePayrollDTO empPayrollDTO);
+
+	EmployeePayrollData updateEmployeePayrollData(int empId, EmployeePayrollDTO empPayrollDTO);
+
+	void deleteEmployeePayrolllData(int empId);
+}
