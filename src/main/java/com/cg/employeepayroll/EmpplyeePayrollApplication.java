f70bfe13e752215063f29c4514ee17e1170f0209
krove <krove@DESKTOP-Q5SED1R>
2020-12-17 03:17:18 +0530

    [Kashif]Welcome to Employee Payroll Spring Backend

---
+++ src/main/java/com/cg/employeepayroll/EmpplyeePayrollApplication.java
@@ -0,0 +1,13 @@
+package com.cg.employeepayroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpplyeePayrollApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpplyeePayrollApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cg/employeepayroll/EmpplyeePayrollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cg.employeepayroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpplyeePayrollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
